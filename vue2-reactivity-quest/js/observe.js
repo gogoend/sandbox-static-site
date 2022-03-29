09882873ff7bbec583b6c04f96f12941ce87283d
gogoend <gogoend@qq.com>
2022-03-29 22:32:07 +0800

    feat: 加入将对象中各个属性转换为响应式属性的逻辑

--- vue2-reactivity-quest/js/observe.js
+++ vue2-reactivity-quest/js/observe.js
@@ -1,4 +1,30 @@
+import { isObj, hasOwn } from "./utils.js";
+
 // 响应式入口
 export const observe = (object) => {
-  console.log(object);
+  if (!isObj(object)) {
+    return;
+  }
+  for (let key in object) {
+    if (hasOwn(object, key)) {
+      defineObserve(object, key);
+    }
+  }
 };
+
+function defineObserve(object, key) {
+  // 当前的属性值为
+  let value = object[key];
+  // 为当前属性值添加响应式
+  observe(value);
+  Object.defineProperty(object, key, {
+    set(val) {
+      value = val;
+    },
+    get() {
+      return value;
+    },
+    enumerable: true,
+    configurable: true
+  });
+}
---
+++ vue2-reactivity-quest/js/utils.js
@@ -0,0 +1,16 @@
+export function isObj(val) {
+  return val && typeof val === "object";
+}
+
+export function remove(arr, item) {
+  if (arr.length) {
+    const index = arr.indexOf(item);
+    if (index > -1) {
+      return arr.splice(index, 1);
+    }
+  }
+}
+
+export function hasOwn(object, key) {
+  return Object.prototype.hasOwnProperty.call(object, key);
+}
