18dce9c5c4feed884c94a32b419d62271c5c83d5
gogoend <gogoend@qq.com>
2022-03-30 16:26:47 +0800

    refactor: 将为对象增加响应式属性的逻辑给放到Observer类中

--- vue2-reactivity-quest/js/observe.js
+++ vue2-reactivity-quest/js/observe.js
@@ -7,12 +7,41 @@ export const observe = (object) => {
   if (!isObj(object)) {
     return;
   }
-  for (let key in object) {
-    if (hasOwn(object, key)) {
-      defineObserve(object, key);
+  let ob;
+  // 如果对象已经进行过响应式处理，就复用已有的Observe对象
+  if (hasOwn(object, '__ob__') && object.__ob__ instanceof Observer) {
+    ob = object.__ob__;
+  }
+  // 如果没有，且对象可以扩展，就新建一个Observe对象
+  else if (Object.isExtensible(object)) {
+    ob = new Observer(object);
+  }
+  return ob;
+};
+
+// 
+class Observer {
+  val
+  constructor (val) {
+    this.val = val
+
+    // 在当前属性上定义一个__ob__属性，以标识出该对象已经是响应式对象
+    // 也就是说使用这个类的时候，会对原始值进行更改
+    Object.defineProperty(val, "__ob__", {
+      value: this,
+      enumerable: false
+    });
+    // 遍历对象下的key，逐个转换为响应式属性
+    this.walk(val)
+  }
+  walk (val) {
+    for (let key in val) {
+      if (hasOwn(val, key)) {
+        defineObserve(val, key);
+      }
     }
   }
-};
+}
 
 function defineObserve(object, key) {
   // 当前的属性值为
