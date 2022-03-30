44da7673ed1e4419e89c0a10941421e73e5f26d0
gogoend <gogoend@qq.com>
2022-03-30 16:26:21 +0800

    feat(依赖管理器): 加入依赖管理器类

---
+++ vue2-reactivity-quest/js/dep.js
@@ -0,0 +1,27 @@
+import { remove } from "./utils.js";
+
+export class Dep {
+  static dId = 0
+
+  constructor() {
+    this.id = Dep.dId++
+    this.subs = [];
+  }
+  addSub(sub) {
+    this.subs.push(sub);
+  }
+  removeSub(sub) {
+    remove(this.subs, sub);
+  }
+  notify() {
+    const subs = this.subs.concat();
+    for (var i = 0, l = subs.length; i < l; i++) {
+      try {
+        subs[i]();
+      } catch (err) {
+        console.err(err);
+        break;
+      }
+    }
+  }
+}
--- vue2-reactivity-quest/js/observe.js
+++ vue2-reactivity-quest/js/observe.js
@@ -1,5 +1,7 @@
 import { isObj, hasOwn } from "./utils.js";
 
+import { Dep } from "./dep.js";
+
 // 响应式入口
 export const observe = (object) => {
   if (!isObj(object)) {
@@ -17,11 +19,17 @@ function defineObserve(object, key) {
   let value = object[key];
   // 为当前属性值添加响应式
   observe(value);
+  // 每个属性对应一个依赖管理器
+  let dep = new Dep();
   Object.defineProperty(object, key, {
     set(val) {
       value = val;
+      // 当属性被更改后，应该通知到该属性的所有依赖
+      dep.notify();
     },
     get() {
+      // TODO: 首次访问该属性时，应该让依赖被收集到
+      // ...
       return value;
     },
     enumerable: true,
